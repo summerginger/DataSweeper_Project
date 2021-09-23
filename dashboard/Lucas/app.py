2a1c5548fd409a4f31a50504e5a6a964d66de5f2
Lucas Chandra <lucas.chandra@outlook.com>
2021-09-22 20:44:21 -0400

    update app.py

--- dashboard/Lucas/app.py
+++ dashboard/Lucas/app.py
@@ -1,4 +1,8 @@
+from pickle import load
 from flask import Flask, render_template, redirect, url_for, request
+import pandas as pd
+import pickle
+
 
 app = Flask(__name__)
 
@@ -13,10 +17,29 @@ def prediction_form():
 @app.route('/result/', methods = ['POST', 'GET'])
 def result():
     if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+        return f"The URL /data is accessed directly. Try going to '/prediction_form' to submit form"
     if request.method == 'POST':
         form_data = request.form
-        return render_template('result.html',form_data = form_data)
+        user_info = []
+        for key,value in form_data.items():
+            user_info.append({key:value})
+        
+        # user_info = pd.DataFrame(user_info)
+
+        # # Import pickle library
+ 
+
+
+        filename = 'randomForest_SMOTE_new.sav'
+        loaded_dict = pickle.load(open(filename, 'rb'))
+        model = loaded_dict["model"]
+        scaler = loaded_dict["scaler"]
+        scaled_data = scaler.transform(user_info)
+        
+        predictions = model.predict(scaled_data)
+
+        return render_template('result.html',predictions = predictions)
 
 if __name__ == "__main__":
    app.run()
+
